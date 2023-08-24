b929fd6550d61116de38348da62cdd904358b790
cotruongsg <cotruongsg@gmail.com>
2023-08-23 18:10:26 -0700

    Modify unroll ex

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,41 +1,18 @@
 function unroll(square) {
-  const result = [];
-
-  let top = 0;
-  let bottom = square.length - 1;
-  let left = 0;
-  let right = square[0].length - 1;
-
-  while (top <= bottom && left <= right) {
-    // Traverse top row
-    for (let i = left; i <= right; i++) {
-      result.push(square[top][i]);
-    }
-    top++;
-
-    // Traverse right column
-    for (let i = top; i <= bottom; i++) {
-      result.push(square[i][right]);
-    }
-    right--;
-
-    // Traverse bottom row
-    if (top <= bottom) {
-      for (let i = right; i >= left; i--) {
-        result.push(square[bottom][i]);
+  const result = []; 
+  
+  while (square.length) {
+    result.push(...square.shift()); // Move right
+    square.forEach(row => row.length && result.push(row.pop())); // Move down
+    square.length && result.push(...square.pop().reverse()); // Move left
+    // Move up 
+    for (let i = square.length - 1; i >= 0; i--) {
+      if (square[i].length) {
+        result.push(square[i].shift());
       }
-      bottom--;
-    }
-
-    // Traverse left column
-    if (left <= right) {
-      for (let i = bottom; i >= top; i--) {
-        result.push(square[i][left]);
-      }
-      left++;
     }
   }
-
+  
   return result;
 }
 
